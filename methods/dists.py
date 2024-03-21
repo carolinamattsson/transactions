bug fix and renaming for activity:  * in scaling the iet by the activity, the inverse is needed  * pass 'mean_iet' to shift the mean activity accordingly (const and pareto)  * clearer kwargs names

methods/dists.py
@@ -9,24 +9,25 @@ from scipy import stats
 
 import pycop.simulation as cop
 
-def random_pareto(N, beta=2.0, mean=1):
+def random_pareto(N, beta=2.0, mean_iet=1):
     '''
     Generate a vector size N with pareto distributed values 
     Pareto: f(x,β) = β / x^(β+1) scaled & shifted such that the mean is 'mean'
     The x_min corresponds to that on wikipedia, where alpha is used instead of beta
     '''
     assert beta > 1, "The shape parameter must be greater than 1."
+    assert mean_iet > 0, "The mean inter-event time must be greater than 1."
+    mean_act = 1/mean_iet
     if beta == np.inf:
-        return np.full(N, mean)
-    x_min = (beta-1)/beta # getting the average activity to be 'mean'
+        return np.full(N, mean_act)
+    x_min = (beta-1)/beta # getting the average activity to be 1
+    pareto = stats.pareto(beta,scale=mean_act*x_min)
     unif = np.random.random(N)
-    #pareto = stats.pareto(beta,loc=mean*(x_min-1),scale=mean)
-    pareto = stats.pareto(beta,scale=mean*x_min)
     pwl = pareto.ppf(unif)
     # now return
     return pwl
 
-def random_paretos(N, betas=(2.0,2.0), means=(1,1), **kwargs):
+def random_paretos(N, betas=(2.0,2.0), means_iet=(1,1), **kwargs):
     '''
     Generate a vector size N with pareto distributed values 
     Pareto: f(x,β) = β / x^(β+1) scaled & shifted such that the mean is 'mean'
@@ -34,17 +35,20 @@ def random_paretos(N, betas=(2.0,2.0), means=(1,1), **kwargs):
     '''
     assert betas[0] > 1, "The shape parameter must be greater than 1."
     assert betas[1] > 1, "The shape parameter must be greater than 1."
+    assert means_iet[0] > 0, "The mean inter-event time must be greater than 1."
+    assert means_iet[1] > 0, "The mean inter-event time must be greater than 1."
+    means_act = (1/means_iet[0], 1/means_iet[1])
     x_min = ((betas[0]-1)/betas[0], (betas[1]-1)/betas[1]) # getting the average activity to be 'mean'
     if betas[0] == np.inf and betas[1] == np.inf:
-        return np.full(N, means[0]), np.full(N, means[1])
+        return np.full(N, means_act[0]), np.full(N, means_act[1])
     elif betas[0] == np.inf:
-        return np.full(N, means[0]), random_pareto(N, betas[1], means[1])
+        return np.full(N, means_act[0]), random_pareto(N, betas[1], means_iet[1])
     elif betas[1] == np.inf:
-        return random_pareto(N, betas[0], means[0]), np.full(N, means[1])
+        return random_pareto(N, betas[0], means_iet[0]), np.full(N, means_act[1])
     else:
         unif_1, unif_2 = random_unifs(N, **{k: kwargs[k] for k in kwargs.keys() & {'copula', 'reversed', 'theta', 'resample'}})
-        pareto_1 = stats.pareto(betas[0],scale=means[0]*x_min[0])
-        pareto_2 = stats.pareto(betas[1],scale=means[1]*x_min[1])
+        pareto_1 = stats.pareto(betas[0],scale=means_act[0]*x_min[0])
+        pareto_2 = stats.pareto(betas[1],scale=means_act[1]*x_min[1])
         pwl_1 = pareto_1.ppf(unif_1)
         pwl_2 = pareto_2.ppf(unif_2)
         # now return them both

methods/model.py
@@ -6,38 +6,38 @@ import heapq as hq
 
 from methods.dists import random_pareto, random_paretos, random_pwl, random_pwls_perturb, random_unifs, random_pwls
 
-def create_nodes(N, scaling="const", coupling=False, **kwargs):
+def create_nodes(N, activity="const", coupling=False, **kwargs):
     '''
     Initialize the underlying of size N with:
-    activity distr:  f(x,β) = β / x^(β+1) with possible scale & shift (see scipy.stats.pareto)
+    Activity distr:  f(x,β) = β / x^(β+1) with possible scale & shift (see scipy.stats.pareto)
     if theta: activity/attractivity joint distribution with an available copula (see methods.dists.random_unifs)
     # leave open the possibility of adding global network constraints (e.g. SBM, ABM)
     '''
     # create activity and attractivity vectors    
     if not coupling:
-        if scaling=="pareto":
-            act_vect = random_pareto(N, **{k: kwargs[k] for k in kwargs.keys() & {'beta', 'mean'}})
+        if activity=="pareto":
+            act_vect = random_pareto(N, **{k: kwargs[k] for k in kwargs.keys() & {'beta', 'mean_iet'}})
             attr_vect = act_vect
-        elif scaling=="pwl":
+        elif activity=="pwl":
             act_vect = random_pwl(N, **{k: kwargs[k] for k in kwargs.keys() & {'beta', 'loc', 'scale'}})
             attr_vect = act_vect
-        elif scaling=="unif":
+        elif activity=="unif":
             act_vect = np.random.random(N)
             attr_vect = act_vect
-        elif scaling=="const":
-            act_vect = np.ones(N)
+        elif activity=="const":
+            act_vect = (1/kwargs['mean_iet'])*np.ones(N) if 'mean_iet' in kwargs else np.ones(N)
             attr_vect = act_vect
         else:
-            raise ValueError("Scaling must be 'pareto' or 'pwl' or 'unif' or 'const'.")
+            raise ValueError("Activity distribution must be 'pareto' or 'pwl' or 'unif' or 'const'.")
     else:
-        if scaling=="pareto":
-            act_vect, attr_vect = random_paretos(N, **{k: kwargs[k] for k in kwargs.keys() & {'betas', 'means', 'copula', 'reversed', 'theta', 'resample'}})
-        if scaling=="pwl":
+        if activity=="pareto":
+            act_vect, attr_vect = random_paretos(N, **{k: kwargs[k] for k in kwargs.keys() & {'betas', 'means_iet', 'copula', 'reversed', 'theta', 'resample'}})
+        if activity=="pwl":
             act_vect, attr_vect = random_pwls(N, **{k: kwargs[k] for k in kwargs.keys() & {'copula', 'reversed', 'theta', 'resample'}})
-        elif scaling=="unif":
+        elif activity=="unif":
             act_vect, attr_vect = random_unifs(N, **{k: kwargs[k] for k in kwargs.keys() & {'copula', 'reversed', 'theta', 'resample'}})
         else:
-            raise ValueError("Scaling must be 'pwl' or 'unif'. Cannot use 'const' with coupling.")
+            raise ValueError("Activity distribution must be 'pareto' or 'pwl' or 'unif'. Cannot use 'const' with coupling.")
     # create dictionary of nodes
     nodes = {i:{} for i in range(N)}
     for node in nodes:
@@ -64,12 +64,12 @@ def initialize_attractivities(nodes):
     attractivities = {node:nodes[node]["attr"]/total for node in nodes}
     return attractivities
 
-def initialize_balances(nodes,monies=100,distribution=np.ones):
+def initialize_balances(nodes,balances=lambda x: 100*np.ones(x)):
     '''
     Initialize the balances for the given nodes
     ''' 
     # create a dictionary of balances, keyed by node
-    bal_vect = monies*distribution(len(nodes))
+    bal_vect = balances(len(nodes))
     balances = {node:bal_vect[node] for node in nodes}
     return balances
 
@@ -78,7 +78,8 @@ def activate(now,activity,distribution):
     Get the next activation time for the given node
     '''
     # draw inter-event time from the relevant distribution
-    next = now + activity*distribution()  # need to pass the parameter(s)
+    mean_iet = 1/activity # invert the activity
+    next = now + mean_iet*distribution()  # need to pass the parameter(s)
     return next
 
 def select(attractivities):