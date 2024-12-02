a84b8abf0d4322450ef04af9045ad90d0c387dd8
claudcelle <claudio.cellerini@gmail.com>
2024-12-02 18:13:41 +0100

    pay random share integration

--- methods/model.py
+++ methods/model.py
@@ -45,7 +45,7 @@ def create_nodes(N, activity=1, attractivity=1, spending=0.5, burstiness=1, mean
     for node in nodes:
         nodes[node]["act"] = activity[node]
         nodes[node]["att"] = attractivity[node]
-        nodes[node]["pay"] = spending[node]
+        nodes[node]["spr"] = spending[node]
         nodes[node]["iet"] = (1./(math.gamma(1+1/burstiness[node])),burstiness[node])
     # return the node dictionary
     return nodes
@@ -103,27 +103,29 @@ def select(attractivities,rng=np.random.default_rng()):
     node_j = rng.choice(list(attractivities.keys()), p=list(attractivities.values()))
     return node_j
 
-def pay_random_share(node_i, node_j, params, balances, rng=np.random.default_rng()):
+def pay_random_share(node_i, node_j, balances, params=None, rng=np.random.default_rng()):
     '''
     Pay the selected node a random share of the available balance:
         - If the balance is continuous, the transaction size is a Beta sampled fraction.
-        - If the balance is discrete, the transaction size is a Beta Binomial sample.
-
-    Example:
-        For balances[0] = Decimal('12.34') with exp = -2:
-            12.34 -> 1234 (scaled), random sample -> 1100, rescaled -> 11.00.
-        For balances[1] = Decimal('123.4') with exp = -1:
-            123.4 -> 1234 (scaled), random sample -> 1200, rescaled -> 120.0.
+        - If the balance is discrete, the transaction size is a Beta Binomial sample.    
 
     TODO: This function is not currently accessible in the model.
     TODO: This would require initializing the params (beta_a, beta_b) instead of p.
+            BetaBinom(n,a,b) = BetaBinom(n,ps,(1-p)s)
     '''
+    if params is None:
+        params = {"p": 0.5, "s": 1.0}
+    # print(f"pay_random_share called with {node_i}, {node_j}, params={params}")
     # sample transaction weight
-    beta_a, beta_b = params
+    # beta_a, beta_b = params
+    p = params.get('p', 0.5)
+    s = params.get('s', 1.0)
+    beta_a, beta_b = p * s, (1 - p) * s
     if isinstance(balances[node_i],Decimal):
         exp = balances[node_i].as_tuple().exponent # -(number of decimal places)
-        txn_size_dist = betabinom(balances[node_i].scaleb(-exp),beta_a,beta_b) # integer valued distribution
-        txn_size = txn_size_dist().rvs() # sample from the distribution
+        n = int(balances[node_i].scaleb(-exp))
+        txn_size_dist = betabinom(n,beta_a,beta_b) # integer valued distribution
+        txn_size = txn_size_dist.rvs() # sample from the distribution
         txn_size = Decimal(txn_size).scaleb(exp) # integer to decimal (e.g.: 1234 -> 12.34)
     else:
         txn_size = balances[node_i]*rng.beta(beta_a,beta_b)
@@ -138,6 +140,12 @@ def pay_share(node_i, node_j, share, balances, rng=np.random.default_rng()):
     Pay the selected node a share of the available balance:
         - If the balance is continuous, the transaction size is a fixed fraction.
         - If the balance is discrete, the transaction size is a Binomial sample.
+
+        Example:
+        For balances[0] = Decimal('1234.56') with exp = -2:
+            1234.56 -> 123456 (scaled), random sample -> 1100, rescaled -> 11.00 .
+        For balances[1] = Decimal('123.4') with exp = -1:
+            123.4 -> 1234 (scaled), random sample -> 12, rescaled -> 1.2 .
     '''
     # sample transaction weight
     if isinstance(balances[node_i],Decimal):
@@ -151,6 +159,41 @@ def pay_share(node_i, node_j, share, balances, rng=np.random.default_rng()):
     balances[node_j] += txn_size
     # return the transaction size
     return txn_size
+
+DEFAULTS = {
+    "random_share": {"p": 1, "s": 0.5},
+    "share": {"share": 0.1},
+}
+
+def get_default_params(method):
+    return DEFAULTS.get(method, {}).copy()
+
+
+def transact(nodes,activations,attractivities,balances,method="share",**kwargs):
+    '''
+    Simulate the next transaction    '''
+
+    # select next active node from the heap
+    now, node_i = hq.heappop(activations)
+    # have the node select a target to transact with
+    node_j = select(attractivities)
+    # pay the target node
+    if method=="random_share":
+        params = kwargs.get("pay_params", get_default_params(method))
+        amount = pay_random_share(node_i, node_j, balances, params)
+    else:
+        amount = pay_share(node_i, node_j, nodes[node_i]["spr"], balances)
+    # update the next activation time for the node
+    next = activate(now,nodes[node_i]["act"],nodes[node_i]["iet"])
+    hq.heappush(activations,(next, node_i))
+    # return the transaction, the updated balances, and the updated activations
+    return {"timestamp":now,
+            "source":node_i,
+            "target":node_j,
+            "amount":amount,
+            "source_bal":balances[node_i],
+            "target_bal":balances[node_j]}
+
 
 def interact(nodes,activations,attractivities,iet=np.random.exponential):
     '''
@@ -167,30 +210,6 @@ def interact(nodes,activations,attractivities,iet=np.random.exponential):
     return {"timestamp":now,
             "source":node_i,
             "target":node_j}
-
-def transact(nodes,activations,attractivities,balances,method="share"):
-    '''
-    Simulate the next transaction
-    '''
-    # select next active node from the heap
-    now, node_i = hq.heappop(activations)
-    # have the node select a target to transact with
-    node_j = select(attractivities)
-    # pay the target node
-    if method=="random_share":
-        amount = pay_random_share(node_i, node_j, nodes[node_i]["pay"], balances)
-    else:
-        amount = pay_share(node_i, node_j, nodes[node_i]["pay"], balances)
-    # update the next activation time for the node
-    next = activate(now,nodes[node_i]["act"],nodes[node_i]["iet"])
-    hq.heappush(activations,(next, node_i))
-    # return the transaction, the updated balances, and the updated activations
-    return {"timestamp":now,
-            "source":node_i,
-            "target":node_j,
-            "amount":amount,
-            "source_bal":balances[node_i],
-            "target_bal":balances[node_j]}
 
 def run_interactions(N,T):
     '''
